136941ac00fe3e12d03b0279e091c80d1eb6b38b
Cedricium <amaya.cedric@gmail.com>
2019-06-19 21:42:53 -0700

    Scaffold basic todo app making use of Redux + Hooks

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { addTodo, toggleTodo } from './actions'
 
-function App() {
+function App(props) {
+  const [todo, setTodo] = useState('')
+
+  const handleSubmitTodo = e => {
+    e.preventDefault()
+    props.addTodo({
+      value: todo,
+      completed: false,
+    })
+    setTodo('')
+  }
+
   return (
     <div className="App">
+      <h2>Get Sh*t Done</h2>
+      <form onSubmit={e => handleSubmitTodo(e)}>
+        <input
+          type="text"
+          value={todo}
+          onChange={e => setTodo(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="Add Todo"
+        />
+      </form>
+      <div>
+        {props.todos.map((todo, index) => (
+          <p key={index}>{todo.value}</p>
+        ))}
+      </div>
     </div>
   )
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos,
+  }
+}
+
+export default connect(mapStateToProps, { addTodo, toggleTodo, })(App)
